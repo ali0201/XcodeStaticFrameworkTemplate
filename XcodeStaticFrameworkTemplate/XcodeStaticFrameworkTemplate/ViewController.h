32cdb268ac59be1eebbdad38543a8acc38a1fe40
Kevin <kevin@ganguo.hk>
2016-06-03 16:07:01 +0800

    first commit

---
+++ XcodeStaticFrameworkTemplate/XcodeStaticFrameworkTemplate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XcodeStaticFrameworkTemplate
+//
+//  Created by ZouJianMing on 16/6/3.
+//  Copyright © 2016年 Kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
