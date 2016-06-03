17c46b88cb7855878e95f9cb0a63256db018380f
Kevin <kevin@ganguo.hk>
2016-06-03 16:08:35 +0800

    add placeholder object

---
+++ XcodeStaticFrameworkTemplate/XcodeStaticFrameworkTemplate/PlaceHolder.h
@@ -0,0 +1,13 @@
+//
+//  PlaceHolder.h
+//  XcodeStaticFrameworkTemplate
+//
+//  Created by ZouJianMing on 16/6/3.
+//  Copyright © 2016年 Kevin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlaceHolder : NSObject
+
+@end
---
+++ XcodeStaticFrameworkTemplate/XcodeStaticFrameworkTemplate/PlaceHolder.m
@@ -0,0 +1,13 @@
+//
+//  PlaceHolder.m
+//  XcodeStaticFrameworkTemplate
+//
+//  Created by ZouJianMing on 16/6/3.
+//  Copyright © 2016年 Kevin. All rights reserved.
+//
+
+#import "PlaceHolder.h"
+
+@implementation PlaceHolder
+
+@end
